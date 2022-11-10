1b98e1090dc3d80dd5e9371c088a8e1b288037ca
leonie zebaze <leoniezebaze@gmail.com>
2022-11-10 14:14:31 +0100

    my commit

---
+++ functions.php
@@ -0,0 +1,14 @@
+<?php 
+
+// Ajouter la prise en charge des images mises en avant
+add_theme_support( 'post-thumbnails' );
+
+// Ajouter automatiquement le titre du site dans l'en-tête du site
+add_theme_support( 'title-tag' );
+
+function additional_custom_styles() {
+
+    /*Enqueue The Styles*/
+    wp_enqueue_style( 'uniquestylesheetid', get_template_directory_uri() . '/css/custom.css' ); 
+}
+add_action( 'wp_enqueue_scripts', 'additional_custom_styles' );
